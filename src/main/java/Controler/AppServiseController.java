5dc5b450254b6f6d6f9cc86a02f2122cb2cb8d29
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-05-06 19:58:24 +0200

    Controller2

--- src/main/java/Controler/AppServiseController.java
+++ src/main/java/Controler/AppServiseController.java
@@ -1,4 +1,33 @@
 package Controler;
 
+import servise.*;
+
+import java.util.Scanner;
+
 public class AppServiseController {
+
+    private final CarServise carServise;
+    private final ClientServise clientServise;
+    private final OrderServise orderServise;
+    private final TypDiagmosticsServise typDiagmosticsServise;
+    private final RepairWorksServise repairWorksServise;
+    private final Scanner scanner;
+
+    public AppServiseController(final CarServise carServise,
+                                final ClientServise clientServise,
+                                final OrderServise orderServise,
+                                final TypDiagmosticsServise typDiagmosticsServise,
+                                final RepairWorksServise repairWorksServise) {
+        this.carServise = carServise;
+        this.clientServise = clientServise;
+        this.orderServise = orderServise;
+        this.typDiagmosticsServise = typDiagmosticsServise;
+        this.repairWorksServise = repairWorksServise;
+        this.scanner=new Scanner(System.in);
+    }
+
+    public void run(){
+        char str;
+        CarRegistraitionControler carRegistraitionControler=new CarRegistraitionControler(carServise,scanner);
+    }
 }
--- src/main/java/Controler/CarRegistraitionControler.java
+++ src/main/java/Controler/CarRegistraitionControler.java
@@ -1,4 +1,15 @@
 package Controler;
 
+import servise.CarServise;
+
+import java.util.Scanner;
+
 public class CarRegistraitionControler {
+    private CarServise servise;
+    private Scanner scanner;
+
+    public CarRegistraitionControler(CarServise servise, Scanner scanner) {
+        this.servise = servise;
+        this.scanner = scanner;
+    }
 }
